a0a90c1dc7a042fb87c8ddd6d1a51bf45c36b8ab
DanillBvc <danylo.makhov@nure.ua>
2023-10-23 14:01:03 +0300

    test

--- client/src/layout/mainLayout/mainLayout.tsx
+++ client/src/layout/mainLayout/mainLayout.tsx
@@ -9,7 +9,6 @@ import { routes } from '../../../App';
 const MainLayout: FC<mainLayoutType> = ({ children, style }) => {
   const [currentRoute, setCurrentRoute] = useState(routes[0].route);
   const route = useRoute();
-
   useEffect(() => {
     setCurrentRoute(route.name);
   }, [route]);
